0f7058579adf4724a9b310d06e196b16b10462a4
JulietaLozanoC <julieta.lo.co@gmail.com>
2025-05-13 21:09:43 -0500

    Update Persona.java

--- pre/src/Modelo/Persona.java
+++ pre/src/Modelo/Persona.java
@@ -9,22 +9,38 @@ package Modelo;
  */
 
 interface Usuario {
-    String getNombre();
+    String verHorario();
+        void mostrarInfo();
+
 }
 abstract  class Persona implements Usuario{
-    protected String nombre;
+   protected String nombre;
+    protected String password;
 
-    public Persona(String nombre) {
+    public Persona(String nombre, String password) {
         this.nombre = nombre;
+        this.password = password;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    @Override
-    public String toString() {
-        return "Persona{" + "nombre=" + nombre + '}';
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+  public boolean verificarPassword(String pass) {
+        return this.password.equals(pass);
+    }
+
+
 
 }
