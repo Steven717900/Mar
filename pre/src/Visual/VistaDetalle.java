d5b06b5d91d2e520eaea517e6ff0beefbce8fe53
JulietaLozanoC <julieta.lo.co@gmail.com>
2025-05-19 11:43:42 -0500

    Update VistaDetalle.java

--- pre/src/Visual/VistaDetalle.java
+++ pre/src/Visual/VistaDetalle.java
@@ -12,18 +12,62 @@ import java.util.*;
  */
 public class VistaDetalle extends JFrame {
 
-    private JTextArea area = new JTextArea();
-
     public VistaDetalle() {
-        setTitle("TutorPlace");
-        setSize(300, 300);
-        setLayout(new FlowLayout());
-        add(new JScrollPane(area), BorderLayout.CENTER);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+
+    public VistaDetalle(Controlador controlador) {
+        super("TutorPlace");
+        setLayout(new GridLayout(5, 1));
+
+        UIManager.put("Button.background", new Color(60, 90, 110));  
+        UIManager.put("OptionPane.messageFont", new Font("Georgia", Font.PLAIN, 14));
+        UIManager.put("OptionPane.buttonFont", new Font("Georgia", Font.PLAIN, 14));
+        UIManager.put("Button.font", new Font("Georgia", Font.PLAIN, 14));
+        UIManager.put("Button.foreground", Color.WHITE);
+        UIManager.put("Button.background", new Color(42, 71, 89));
+
+        JButton BotonInicioEst = new JButton("Iniciar como Estudiante");
+        JButton BotonInicioTutor = new JButton("Iniciar como Tutor");
+        JButton BotonRegEst = new JButton("Registrarse como Estudiante");
+        JButton BotonRegTutor = new JButton("Registrarse como Tutor");
+        JButton BotonVerHorarios = new JButton("Ver todos los horarios");
+        Color colorOriginal = new Color(42, 71, 89);
+        Color colorHover = new Color(60, 100, 130);
+        CambieColor(BotonInicioEst, colorOriginal, colorHover);
+        CambieColor(BotonInicioTutor, colorOriginal, colorHover);
+        CambieColor(BotonRegEst, colorOriginal, colorHover);
+        CambieColor(BotonRegTutor, colorOriginal, colorHover);
+        CambieColor(BotonVerHorarios, colorOriginal, colorHover);
+
+        BotonInicioEst.addActionListener(e -> controlador.InicioSesionUsuario("Estudiante"));
+        BotonInicioTutor.addActionListener(e -> controlador.InicioSesionUsuario("Tutor"));
+        BotonRegEst.addActionListener(e -> controlador.registrarEstudiante());
+        BotonRegTutor.addActionListener(e -> controlador.registrarTutor());
+        BotonVerHorarios.addActionListener(e -> controlador.mostrarHorarios());
+
+        add(BotonInicioEst);
+        add(BotonInicioTutor);
+        add(BotonRegEst);
+        add(BotonRegTutor);
+        add(BotonVerHorarios);
+    }
+
+    public void CambieColor(JButton boton, Color colorOriginal, Color colorCambio) {
+        boton.setBackground(colorOriginal);
+        boton.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                boton.setBackground(colorCambio);
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                boton.setBackground(colorOriginal);
+            }
+        });
     }
 
     public void mostrar(String texto) {
-        area.setText(texto);
-        setVisible(true);
+        JOptionPane.showMessageDialog(null, texto);
     }
 }
